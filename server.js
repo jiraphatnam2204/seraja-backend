68c9e5e4c25aa14e8b081a0aeb6ac4b477c246af
James <jiraphatnam2204@gmail.com>
2026-04-11 12:00:11 +0700

    docs(api-docs): add swagger and yaml

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 const { setServers } = require("node:dns/promises");
 setServers(["1.1.1.1", "8.8.8.8"]);
 
-const express = require('express');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const { xss } = require('express-xss-sanitizer');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("js-yaml");
+const fs = require("fs");
+const swaggerSpec = yaml.load(fs.readFileSync("./openapi.yaml", "utf8"));
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const { xss } = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 connectDB();
 
-const auth = require('./routes/auth');
-const campgrounds = require('./routes/campgrounds');
-const bookings = require('./routes/bookings');
+const auth = require("./routes/auth");
+const campgrounds = require("./routes/campgrounds");
+const bookings = require("./routes/bookings");
 
 const app = express();
 
-app.set('trust proxy', 1)
+app.set("trust proxy", 1);
 
-app.set('query parser', 'extended');
+app.set("query parser", "extended");
 app.use(express.json());
 app.use(cookieParser());
 
@@ -39,21 +43,31 @@ app.use(
     max: 100,
     standardHeaders: true,
     legacyHeaders: false,
-  })
+  }),
 );
 
 app.use(hpp());
 app.use(cors());
 
+app.use(
+  "/api-docs",
+  (req, res, next) => {
+    res.setHeader("Content-Security-Policy", "");
+    next();
+  },
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec),
+);
+
 // routes
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/campgrounds', campgrounds);
-app.use('/api/v1/bookings', bookings);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/campgrounds", campgrounds);
+app.use("/api/v1/bookings", bookings);
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () =>
-  console.log("Server running in", process.env.NODE_ENV, "mode on port", PORT)
+  console.log("Server running in", process.env.NODE_ENV, "mode on port", PORT),
 );
 
 process.on("unhandledRejection", (err) => {
